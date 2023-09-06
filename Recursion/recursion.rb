a1f5690f6956687a01441e754a4e9e58a30df4e2
Omar <ohammad@gwu.edu>
2023-09-06 13:50:33 -0400

    start of exponent

--- Recursion/recursion.rb
+++ Recursion/recursion.rb
@@ -13,4 +13,12 @@ def range_rec(start, finish)
     range_rec(start,finish - 1) + [finish - 1] 
 end
 
-p range_rec(1, 5)
+# p range_rec(1, 5)
+
+def first_exponenet(b, n)
+    return 1 if n == 0
+
+    b * first_exponenet(b, n - 1)
+end
+
+p first_exponenet(3, 2)
